720bd7bd9d97b5f17e746568db4cc3fa133d0fb3
krypniok <61298806+krypniok@users.noreply.github.com>
2020-02-21 01:14:25 +0100

    Add files via upload

---
+++ you2mp3.php
@@ -0,0 +1,19 @@
+#pip install youtube-dl
+#sudo apt autoinstall sed jq youtube-dl ffmpeg
+
+#!/bin/bash
+
+#load the youtube lists URLs to $LINES
+youtube-dl -j --flat-playlist "$1" | jq -r '.id' | sed 's_^_https://youtu.be/_' > result.log
+readarray -t LINES < "result.log"
+rm result.log
+
+for LINE in "${LINES[@]}"; do
+    youtube-dl -q --no-warnings $LINE
+    INFILE="`ls -1cr | tail -n 1`"
+    ffmpeg -loglevel quiet -i "$INFILE" "${INFILE%.*}.mp3"
+    OUTFILE="`ls -1cr | tail -n 1`"
+    unlink "$INFILE"
+    echo $OUTFILE
+done
+
