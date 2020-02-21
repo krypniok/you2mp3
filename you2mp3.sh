2ea1a193f201a15650ea7d808920fc06f71ca0e7
krypniok <61298806+krypniok@users.noreply.github.com>
2020-02-21 01:24:05 +0100

    Update you2mp3.sh

--- you2mp3.sh
+++ you2mp3.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
-#load the youtube lists URLs to $LINES
 youtube-dl -j --flat-playlist "$1" | jq -r '.id' | sed 's_^_https://youtu.be/_' > result.log
 readarray -t LINES < "result.log"
 rm result.log
-
 for LINE in "${LINES[@]}"; do
     youtube-dl -q --no-warnings $LINE
     INFILE="`ls -1cr | tail -n 1`"
@@ -13,4 +10,3 @@ for LINE in "${LINES[@]}"; do
     unlink "$INFILE"
     echo $OUTFILE
 done
-
