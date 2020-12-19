1ff3885e6460bde2107adcc4f8ec7dd6808d424b
krypniok <61298806+krypniok@users.noreply.github.com>
2020-12-19 01:00:10 +0100

    Update you2mp3.sh
    Really works...

--- you2mp3.sh
+++ you2mp3.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 youtube-dl -j --flat-playlist "$1" | jq -r '.id' | sed 's_^_https://youtu.be/_' > result.log
 while IFS='' read -r LINE || [ -n "${LINE}" ]; do
-    echo "processing line: ${LINE}"
+    TITLE=$(youtube-dl --skip-download --get-title --no-warnings ${LINE} | sed 2d)
+    echo "Downloading and converting '$TITLE' to MP3..."
     youtube-dl -q --no-warnings --extract-audio --audio-format mp3 ${LINE}
+    exit
 done < result.log
 rm result.log
