3949d45a32a6506047a4e09ee9ff0c6eac041bb0
krypniok <61298806+krypniok@users.noreply.github.com>
2020-12-19 01:00:41 +0100

    Update you2mp3.sh

--- you2mp3.sh
+++ you2mp3.sh
@@ -4,6 +4,5 @@ while IFS='' read -r LINE || [ -n "${LINE}" ]; do
     TITLE=$(youtube-dl --skip-download --get-title --no-warnings ${LINE} | sed 2d)
     echo "Downloading and converting '$TITLE' to MP3..."
     youtube-dl -q --no-warnings --extract-audio --audio-format mp3 ${LINE}
-    exit
 done < result.log
 rm result.log
